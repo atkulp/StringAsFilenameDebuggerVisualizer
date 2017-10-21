78fc35ee63a7f88a495db5bc4a53291378961a32
Arian Kulp <ariankulp@gmail.com>
2017-10-20 18:16:06 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # StringAsFilenameDebuggerVisualizer
-Debugger visualizer for Visual Studio 2017 to open a file by taking a string variable value as the filename
+## Arian T. Kulp
+Debugger visualizer for Visual Studio 2017 to open a file by taking a string variable value as the filename.
+
+Use prepackaged release or install manually by copying the built DLL (ArianKulp.DebuggerExtensions.dll) to $UserProfile\Documents\Visual Studio 2017\Visualizers.  Though the project contains a VSIX builder and a VSPackage, they are not functional as managed extensions **can't** be installed via VSIX.
+
+Note, this actually adds two new visualizers so you can open the file in Visual Studio (without adding it to the project/solution), or in Notepad.
+
+### Known bugs:
+None at the moment
+
+### Future plans
+- Add settings for choosing text editor other than Notepad
+- Possibly add option to save current string value into temp file and then open it
---
+++ Solution/Extensions/StringOpenFileDebuggerExtension.cs
@@ -0,0 +1,113 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.DebuggerVisualizers;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows.Forms;
+
+[assembly: DebuggerVisualizer(typeof(ArianKulp.DebuggerExtensions.StringOpenFileDebuggerExtension),
+    typeof(VisualizerObjectSource),
+    Target = typeof(System.String),
+    Description = "View file")]
+
+[assembly: DebuggerVisualizer(typeof(ArianKulp.DebuggerExtensions.StringOpenFileInNotepadDebuggerExtension),
+    typeof(VisualizerObjectSource),
+    Target = typeof(System.String),
+    Description = "View file in Notepad")]
+
+namespace ArianKulp.DebuggerExtensions
+{
+    public class StringOpenFileDebuggerExtension : DialogDebuggerVisualizer
+    {
+        override protected void Show(IDialogVisualizerService windowService, IVisualizerObjectProvider objectProvider)
+        {
+            if (windowService == null)
+                throw new ArgumentNullException( nameof(windowService));
+            if (objectProvider == null)
+                throw new ArgumentNullException(nameof(objectProvider));
+
+            var fn = objectProvider.GetObject()?.ToString();
+            var dte2 = ServiceProvider.GlobalProvider.GetService(typeof(DTE)) as DTE2;
+            
+            if (dte2 != null && fn != null)
+            {
+                dte2.ItemOperations.OpenFile(fn);
+            }
+            else
+            {
+                if (ThreadHelper.CheckAccess())
+                {
+                    // This shouldn't ever get here...
+                    IVsUIShell uiShell = (IVsUIShell)ServiceProvider.GlobalProvider.GetService(typeof(SVsUIShell));
+                    Guid clsid = Guid.Empty;
+                    int result;
+                    Microsoft.VisualStudio.ErrorHandler.ThrowOnFailure(uiShell.ShowMessageBox(
+                        0,
+                        ref clsid,
+                        "String visualizer extension",
+                         string.Format(CultureInfo.CurrentCulture, "Sorry, something went wrong.", this.GetType().FullName),
+                        string.Empty,
+                        0,
+                        OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                        OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST,
+                        OLEMSGICON.OLEMSGICON_INFO,
+                        0,
+                        out result));
+                }
+            }
+        }
+
+        public static void TestShowVisualizer(object objectToVisualize)
+        {
+            var visualizerHost = new VisualizerDevelopmentHost(objectToVisualize, typeof(StringOpenFileDebuggerExtension));
+            visualizerHost.ShowVisualizer();
+        }
+    }
+
+    public class StringOpenFileInNotepadDebuggerExtension : DialogDebuggerVisualizer
+    {
+        override protected void Show(IDialogVisualizerService windowService, IVisualizerObjectProvider objectProvider)
+        {
+            var fn = objectProvider.GetObject()?.ToString();
+
+            if (fn != null)
+            {
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo.FileName = "notepad.exe";
+                proc.StartInfo.Arguments = fn;
+                proc.Start();
+            }
+            else
+            {
+                if (ThreadHelper.CheckAccess())
+                {
+                    // This shouldn't ever get here...
+                    IVsUIShell uiShell = (IVsUIShell)ServiceProvider.GlobalProvider.GetService(typeof(SVsUIShell));
+                    Guid clsid = Guid.Empty;
+                    int result;
+                    Microsoft.VisualStudio.ErrorHandler.ThrowOnFailure(uiShell.ShowMessageBox(
+                        0,
+                        ref clsid,
+                        "String visualizer extension",
+                         string.Format(CultureInfo.CurrentCulture, "Sorry, something went wrong.", this.GetType().FullName),
+                        string.Empty,
+                        0,
+                        OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                        OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST,
+                        OLEMSGICON.OLEMSGICON_INFO,
+                        0,
+                        out result));
+                }
+            }
+        }
+
+        public static void TestShowVisualizer(object objectToVisualize)
+        {
+            var visualizerHost = new VisualizerDevelopmentHost(objectToVisualize, typeof(StringOpenFileInNotepadDebuggerExtension));
+            visualizerHost.ShowVisualizer();
+        }
+    }
+}
